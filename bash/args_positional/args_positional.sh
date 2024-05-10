4621e5e50012f6b22cf088ee862de545e60723e8
Arno Becker <42ao@posteo.net>
2024-05-10 11:56:09 +0200

    add bash args_positional

---
+++ bash/args_positional/args_positional.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+required=${1}
+default_empty=${2:-}
+default_value=${3:-default}
+
+if [[ -z ${required} ]]; then
+    echo "Argument 'required' is missing"
+    exit 1
+fi
+
+echo "Required arg          : ${required}"
+echo "Arg with empty default: ${default_empty}"
+echo "Arg with default value: ${default_value}"
---
+++ bash/args_positional/try_args_positional.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# shellcheck disable=SC2016,SC2119
+
+. "${BASH_SOURCE%/*}"/../format/format.sh
+
+print_source "${BASH_SOURCE%/*}"/args_positional.sh
+
+print_title "Execute args_positional.sh without required argument"
+print_code <<'EOF'
+"${BASH_SOURCE%/*}"/args_positional.sh
+echo "Exit code: $?"
+EOF
+"${BASH_SOURCE%/*}"/args_positional.sh
+echo "Exit code: $?"
+
+print_title "Execute args_positional.sh with 1 required argument"
+print_code <<'EOF'
+"${BASH_SOURCE%/*}"/args_positional.sh arg1
+echo "Exit code: $?"
+EOF
+"${BASH_SOURCE%/*}"/args_positional.sh arg1
+echo "Exit code: $?"
+
+print_title "Execute args_positional.sh with 2 arguments"
+print_code <<'EOF'
+"${BASH_SOURCE%/*}"/args_positional.sh arg1 arg2
+echo "Exit code: $?"
+EOF
+"${BASH_SOURCE%/*}"/args_positional.sh arg1 arg2
+echo "Exit code: $?"
+
+print_title "Execute args_positional.sh with 3 arguments"
+print_code <<'EOF'
+"${BASH_SOURCE%/*}"/args_positional.sh arg1 arg2 arg3'
+echo "Exit code: $?"
+EOF
+"${BASH_SOURCE%/*}"/args_positional.sh arg1 arg2 arg3
+echo "Exit code: $?"
