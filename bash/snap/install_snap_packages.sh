3b7b01b2eb5e9c4601e682dba7160ec41b95c0cf
Arno Becker <42ao@posteo.net>
2024-05-11 13:21:43 +0200

    add bash scripts for installing and removing snaps

---
+++ bash/snap/install_snap_packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. "${BASH_SOURCE%/*}"/snap_installed.sh
+
+install_snap_packages() {
+  local packages=("$@")
+  for package_with_options in "${packages[@]}"; do
+    package=${package_with_options%% *}
+    if snap_installed "${package}"; then
+      echo "${package} is already installed."
+    else
+      echo "Installing ${package}..."
+      # shellcheck disable=2086
+      sudo snap install ${package_with_options}
+    fi
+  done
+}
+
+if [[ ${BASH_SOURCE[0]} == "$0" ]]; then
+  install_snap_packages "$@"
+fi
---
+++ bash/snap/remove_snap_packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. "${BASH_SOURCE%/*}"/snap_installed.sh
+
+remove_snap_packages() {
+  local packages=("$@")
+  for package_with_options in "${packages[@]}"; do
+    package=${package_with_options%% *}
+    if ! snap_installed "${package}"; then
+      echo "${package} is not installed."
+    else
+      echo "Removing ${package}..."
+      # shellcheck disable=2086
+      sudo snap remove ${package}
+    fi
+  done
+}
+
+if [[ ${BASH_SOURCE[0]} == "$0" ]]; then
+  remove_snap_packages "$@"
+fi
