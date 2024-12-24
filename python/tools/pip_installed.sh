d1e27aa15b8667076d6666c043c04b3c436977b2
Arno Becker <42ao@posteo.net>
2024-12-24 22:56:21 +0100

    extract pip functions

---
+++ python/tools/pip_installed.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip="pip3"
+
+pip_installed() {
+  local package=$1
+  ${pip} show "${package}" &>/dev/null
+}
+
+if [[ ${BASH_SOURCE[0]} == "$0" ]]; then
+  pip_installed "$1"
+fi
--- python/tools/setup_python_tools.sh
+++ python/tools/setup_python_tools.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
 . "${BASH_SOURCE%/*}"/python_tools.sh
+. "${BASH_SOURCE%/*}"/install_pip_packages.sh
 . "${BASH_SOURCE%/*}"/../../bash/os/get_os.sh
 . "${BASH_SOURCE%/*}"/../../bash/brew/install_brew_packages.sh
-
-pip="pip3"
-
-pip_package_installed() {
-  local package=$1
-  ${pip} show "${package}" &>/dev/null
-}
-
-install_pip_packages() {
-  local packages=("$@")
-  for package in "${packages[@]}"; do
-    if pip_package_installed "${package}"; then
-      echo "${package} is already installed."
-    else
-      echo "Installing ${package}..."
-      ${pip} install "${package}"
-    fi
-  done
-}
 
 setup_python_tools_on_macos() {
   install_brew_packages "${PYTHON_BREW_PACKAGES[@]}"
