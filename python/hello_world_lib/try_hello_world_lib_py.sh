bb9810520b7f397df608ce659c177711a60195f6
Arno Becker <42ao@posteo.net>
2024-12-31 13:11:38 +0100

    align try scripts for python hello_world_lib

--- python/hello_world_lib/try_hello_world_lib_py.sh
+++ python/hello_world_lib/try_hello_world_lib_py.sh
@@ -5,39 +5,40 @@
 . "${BASH_SOURCE%/*}"/../../bash/format/format.sh
 
 python="python3"
+script_path="${BASH_SOURCE%/*}/hello_world_lib.py"
 
-print_source "${BASH_SOURCE%/*}"/hello_world_lib.py
+print_source "$script_path"
 print_code <<<"working directory: $(pwd)"
 
 print_section "execute script via shebang interpreter"
 print_code <<<'./SCRIPT.py [ARGS]'
 
 print_title "without arguments"
-print_code <<<'"${BASH_SOURCE%/*}"/hello_world_lib.py'
-"${BASH_SOURCE%/*}"/hello_world_lib.py
+print_code <<<"$script_path"
+"$script_path"
 
 print_title "with one argument"
-print_code <<<'"${BASH_SOURCE%/*}"/hello_world_lib.py "yourself"'
-"${BASH_SOURCE%/*}"/hello_world_lib.py "yourself"
+print_code <<<"$script_path \"yourself\""
+"$script_path" "yourself"
 
 print_title "with multiple arguments"
-print_code <<<'"${BASH_SOURCE%/*}"/hello_world_lib.py first and last name'
-"${BASH_SOURCE%/*}"/hello_world_lib.py first and last name
+print_code <<<"$script_path first and last name"
+"$script_path" first and last name
 
 print_section "execute script via command line interpreter"
 print_code <<<"${python} ./SCRIPT.py [ARGS]"
 
 print_title "without arguments"
-print_code <<<"${python} \"${BASH_SOURCE%/*}/hello_world_lib.py\""
-${python} "${BASH_SOURCE%/*}/hello_world_lib.py"
+print_code <<<"${python} \"$script_path\""
+${python} "$script_path"
 
 print_title "with one argument"
-print_code <<<"${python} \"${BASH_SOURCE%/*}/hello_world_lib.py\" \"yourself\""
-${python} "${BASH_SOURCE%/*}/hello_world_lib.py" "yourself"
+print_code <<<"${python} \"$script_path\" \"yourself\""
+${python} "$script_path" "yourself"
 
 print_title "with multiple arguments"
-print_code <<<"${python} \"${BASH_SOURCE%/*}/hello_world_lib.py\" first and last name"
-${python} "${BASH_SOURCE%/*}/hello_world_lib.py" first and last name
+print_code <<<"${python} \"$script_path\" first and last name"
+${python} "$script_path" first and last name
 
 print_section "execute package via command line interpreter"
 print_code <<<"${python} ./PACKAGE [ARGS]"
